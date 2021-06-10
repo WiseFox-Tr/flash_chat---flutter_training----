7bb037f8545ec0275f9e5a40f908c8c147d6fe2d
Maxime Traverse <maxime.Traverse@orange.fr>
2021-06-10 18:44:18 +0200

    add a method that convert exception message into a comprehensible error message for user

---
+++ lib/utilities/error_manager.dart
@@ -0,0 +1,18 @@
+import 'package:flash_chat/AppConst.dart';
+
+String getErrorMessageForUser(String exceptionMessage) {
+  if(exceptionMessage.contains('Given String is empty or null') || exceptionMessage.contains('One required attribute is empty'))
+    return AppConst.strErrorEmptyField;
+  else if(exceptionMessage.contains('[firebase_auth/invalid-email]'))
+    return AppConst.strErrorInvalidEMail;
+  else if(exceptionMessage.contains('[firebase_auth/weak-password]'))
+    return AppConst.strErrorWeakPassword;
+  else if(exceptionMessage.contains('[firebase_auth/email-already-in-use]'))
+    return AppConst.strErrorEMailAlreadyInUse;
+  else if(exceptionMessage.contains('[firebase_auth/user-not-found]'))
+    return AppConst.strErrorUserNotFound;
+  else if(exceptionMessage.contains('[firebase_auth/wrong-password]'))
+    return AppConst.strErrorWrongPassword;
+  else
+    return AppConst.strErrorDefaultMessage;
+}
