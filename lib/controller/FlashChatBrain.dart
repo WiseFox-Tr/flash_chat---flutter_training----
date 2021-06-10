8065e3ad8caa47c91141f294482c5b4a1fcb7b0e
Maxime Traverse <maxime.Traverse@orange.fr>
2021-06-10 17:21:41 +0200

    add a register method & a findCurrentUser method + getter for current user mail

--- lib/controller/FlashChatBrain.dart
+++ lib/controller/FlashChatBrain.dart
@@ -1,10 +1,38 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flash_chat/ui/routes.dart';
+import 'package:flutter/cupertino.dart';
+
 class FlashChatBrain {
 
   String _inputMail;
   String _inputPassword;
 
+  final _auth = FirebaseAuth.instance;
+  User _currentUser;
+
+  Future<void> registerUser(BuildContext context) async {
+    try{
+      final newUser = await _auth.createUserWithEmailAndPassword(email: _inputMail, password: _inputPassword);
+      if(newUser != null)
+        Navigator.pushNamed(context, IdScreen.chat_screen);
+    } catch(e) {
+      print('DEBUG : Exception : $e');
+    }
+  }
+
+  void findCurrentUser() {
+    try {
+      _currentUser = _auth.currentUser;
+      if(_currentUser == null)
+        throw Exception('Current user is null !');
+    } catch(e) {
+      print('DEBUG : Exception : $e');
+    }
+  }
+
   get getInputMail => _inputMail;
   get getInputPassword => _inputPassword;
+  get getCurrentUserMail => _currentUser.email;
   set setInputMail(String newMail) => _inputMail = newMail;
   set setInputPassword(String newPassword) => _inputPassword = newPassword;
 }
