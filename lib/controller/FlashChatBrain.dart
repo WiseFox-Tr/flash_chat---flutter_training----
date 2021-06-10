aee10e06511c31971790829451d90b0d66e6be3d
Maxime Traverse <maxime.Traverse@orange.fr>
2021-06-10 15:21:38 +0200

    add a Brain class with a mail & password attribute + getters & setters

---
+++ lib/controller/FlashChatBrain.dart
@@ -0,0 +1,10 @@
+class FlashChatBrain {
+
+  String _inputMail;
+  String _inputPassword;
+
+  get getInputMail => _inputMail;
+  get getInputPassword => _inputPassword;
+  set setInputMail(String newMail) => _inputMail = newMail;
+  set setInputPassword(String newPassword) => _inputPassword = newPassword;
+}
