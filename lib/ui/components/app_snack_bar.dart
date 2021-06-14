4394d27a7b1eeb48c0d4138210ec391fc1ba5a83
Maxime Traverse <maxime.Traverse@orange.fr>
2021-06-14 19:31:44 +0200

    update snackbar style

--- lib/ui/components/app_snack_bar.dart
+++ lib/ui/components/app_snack_bar.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 
 SnackBar getSnackBar(String message) {
   return SnackBar(
-    backgroundColor: Colors.red,
+    backgroundColor: Colors.blueAccent,
     duration: Duration(seconds: 3),
     content: Text(
       message,
       textAlign: TextAlign.center,
       style: TextStyle(
         color: Colors.white,
-        fontSize: 18.0,
+        fontSize: 14.0,
         fontWeight: FontWeight.bold,
       ),
     ),
